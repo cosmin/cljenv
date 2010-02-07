dd3b94ee9853a1808961fef83089cad922e02ec3
Cosmin Stejerean <cosmin@offbytwo.com>
2010-02-06 23:40:44 -0600

    fixed install.sh to print instructions instead, updated README

--- install.sh
+++ install.sh
@@ -3,22 +3,21 @@
 OS="`uname`"
 INSTALL_LOCATION="`pwd`"
 
-if [[ $OS = "Darwin" ]]; then
-    BASHRC="~/.profile"
-else
-    BASHRC="~/.bashrc"
-fi
-
 echo "Initializing submodules"
 git submodule update --init
 touch ~/.clj_completions # this file must exist or bad things happen
+echo ""
 
-echo "source $INSTALL_LOCATION/setup.sh" >> $BASHRC
-echo "cljenv_autostart NOPS1" >> $BASHRC
-echo "Added CLJENV setup.sh to be sourced by default"
+echo "= Put the following in your .bashrc or similar ="
+echo ""
+echo "source $INSTALL_LOCATION/setup.sh"
+echo "cljenv_autostart NOPS1"
+echo ""
+echo ""
 
-echo "(load (concat (getenv \"CLJENV_HOME\") \"/\" \"setup.el\"))" >> ~/.emacs
-echo "Added Shared Profile setup.el to Emacs"
-
+echo "= Put the following in your .emacs to get emacs support"
+echo ""
+echo "(load (concat (getenv \"CLJENV_HOME\") \"/\" \"setup.el\"))"
+echo ""
 echo ""
 echo "Done."
