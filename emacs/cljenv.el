c076b0568956e3956592104d858c3f1be3d28e8f
Cosmin Stejerean <cosmin@offbytwo.com>
2010-02-06 23:08:32 -0600

    replaced CLOJURE_JAR to CLJENV_HOME in cljenv.el

--- emacs/cljenv.el
+++ emacs/cljenv.el
@@ -9,7 +9,7 @@
   (add-to-list 'load-path (concat cljenv-emacs-path "swank-clojure/"))
   (add-to-list 'load-path (concat cljenv-emacs-path "clojure-mode/"))
 
-  (if (getenv "CLOJURE_JAR")
+  (if (getenv "CLJENV_HOME")
       (progn
         (add-to-list 'load-path (concat shared-profile-elisp "swank-clojure/"))
         (add-to-list 'load-path (concat shared-profile-elisp "clojure-mode/"))
